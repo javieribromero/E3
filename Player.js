e0de4ed0fa8175d69a029198d1a161cee640e457
Javier Ibañez <47353659+javieribromero@users.noreply.github.com>
2020-06-11 11:16:06 +0200

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,77 @@
+/**
+ * Personaje principal del juego. Hereda de la clase Character.
+ * @extends Character
+ */
+class Player extends Character {
+    /**
+     * Inicializa un jugador
+     * @param game {Game} La instancia del juego al que pertenece el personaje
+     */
+    constructor(game) {
+        const height = PLAYER_HEIGHT * game.width / 100,
+            width = PLAYER_WIDTH * game.width / 100,
+            x = game.width / 2 - width / 2,
+            y = game.height - height,
+            speed = PLAYER_SPEED,
+            myImage = PLAYER_PICTURE,
+            myImageDead = PLAYER_PICTURE_DEAD;
+
+        super(game, width, height, x, y, speed, myImage, myImageDead);
+        this.lives = 3;
+    }
+
+    /**
+     * Actualiza los atributos de posición del jugador y los disparos en función de las teclas pulsadas
+     */
+    update() {
+        if (!this.dead) {
+            switch (this.game.keyPressed) {
+                case KEY_LEFT:
+                    if (this.x > this.speed) {
+                        this.x -= this.speed;
+                    }
+                    break;
+                case KEY_RIGHT:
+                    if (this.x < this.game.width - this.width - this.speed) {
+                        this.x += this.speed;
+                    }
+                    break;
+                case KEY_SHOOT:
+                    this.game.shoot(this);
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Mata al jugador
+     */
+    die() {
+
+        this.lives--;
+        if (this.lives == 0) {
+            if (!this.dead) {
+                setTimeout(() => {
+                    this.game.endGame();
+                }, 2000);
+                super.die();
+            }
+            // console.log(this.lives);
+        } else {
+            this.dead = true;
+            this.myImage.src = this.myImageDead;
+            this.dead=true;
+            setTimeout(() => {
+                this.dead = false;
+                this.myImage.src = PLAYER_PICTURE;
+            }, 2000);
+           console.log(this.lives);
+        }
+ //       if (!this.dead) {
+ //           setTimeout(() => {
+ //               this.game.endGame();
+ //           }, 2000);
+  //          super.die();
+  //      }
+    }
+}
